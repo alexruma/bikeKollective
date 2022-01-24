15ae6acf0ed8bbd74b74bea8f9dcaf2bd90c9582
mxjeffers <30639610+mxjeffers@users.noreply.github.com>
2022-01-24 10:30:51 -0700

    Firestore and bike markers
    Able to communicate with firestore. Bike markers for the bike locations are now viewable on the map.

--- myapp/lib/src/gmaps.dart
+++ myapp/lib/src/gmaps.dart
@@ -3,6 +3,8 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:http/http.dart' as http;
 import 'constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 
 
@@ -16,6 +18,10 @@ class Gmaps extends StatefulWidget {
 class _GmapsState extends State<Gmaps> {
 
   late GoogleMapController mapController;
+  List<Marker> _markers = <Marker>[];
+  //Stream collectionStream = FirebaseFirestore.instance.collection('bikes').snapshots();
+  var markers;
+  final Stream<QuerySnapshot> bikes = FirebaseFirestore.instance.collection('bikes').snapshots();
 
   // Starting position of the map
   // Location is Oregon state university
@@ -26,6 +32,7 @@ class _GmapsState extends State<Gmaps> {
 
   // Startup tasks when map is created
   void _onMapCreated(GoogleMapController controller){
+    createMarkers();
     mapController = controller;
   }
 
@@ -42,6 +49,36 @@ class _GmapsState extends State<Gmaps> {
     super.dispose();
   }
 
+
+  createMarkers() async {
+    //Markers for Bike available Locations
+    // Calls firestore and gets bike info
+    markers = [];
+    FirebaseFirestore.instance.collection('bikes').get()
+        .then((docs) {
+      docs.docs.forEach((element) {
+        //print(element['make']);
+        initMarker(element);
+      });
+    });
+
+
+
+  }
+
+  initMarker(bike){
+    // Set state neede to update markers on Gmap
+    setState(() {
+    _markers.add(Marker(
+        markerId: MarkerId(bike['make']),
+        position: LatLng(bike['location'].latitude,
+            bike['location'].longitude),
+        infoWindow: InfoWindow(title: bike['model'])
+    ));
+    });
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,7 +87,7 @@ class _GmapsState extends State<Gmaps> {
         appBar: AppBar(title: const Text("Bike Kollective"),),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
+        children: [
           Expanded(
             flex: 14,
             child: GoogleMap(
@@ -60,8 +97,44 @@ class _GmapsState extends State<Gmaps> {
               zoomControlsEnabled: true,
               initialCameraPosition:
                 CameraPosition(target: _center, zoom: 16),
+              markers: Set<Marker>.of(_markers),
             ),
-          )
+          ),
+          Expanded(flex: 5,
+          child:
+          StreamBuilder<QuerySnapshot>(stream: bikes,
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot){
+            if (snapshot.hasError){
+              return Text('Something went wrong.');}
+            if (snapshot.connectionState == ConnectionState.waiting){
+              return Text('Loading');
+            }
+            final data = snapshot.requireData;
+            var items = snapshot.data?.docs ?? [];
+
+            //Stream which updates markers on change
+            // Will Use later
+            // items.forEach((element) {
+            //   print(element['location'].latitude);
+            //   _markers.add(Marker(
+            //     markerId: MarkerId(element['make']),
+            //     position: LatLng(element['location'].latitude,
+            //               element['location'].longitude),
+            //     infoWindow: InfoWindow(title: element['model'])
+            //   ));
+
+            // });
+
+
+            return ListView.builder(
+                itemCount: data.size,
+                itemBuilder: (context, index){
+
+                  return Text('This bike is ${data.docs[index]['make']}');
+                });
+
+          }))
         ],
       ),
     );
