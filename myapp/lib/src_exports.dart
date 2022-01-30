bc3d09a3813043016afa8f598b7f162b962e489e
lucaono13 <gionorato@gmail.com>
2022-01-30 14:05:18 -0800

    Updated gitignore and src_exports.dart

--- myapp/lib/src_exports.dart
+++ myapp/lib/src_exports.dart
@@ -1,2 +1,7 @@
 export 'src/gmaps.dart';
 export 'src/home.dart';
+export 'src/login_page.dart';
+export 'src/navbar.dart';
+export 'src/gmaps.dart';
+export 'src/create_account.dart';
+export 'src/constants.dart';
