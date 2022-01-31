88748ee813f1c2c2e71acbe6d167ad02979ba9c9
spencer <spencerwagner0@gmail.com>
2022-01-30 23:39:49 -0800

    Revert "Splash screen works properly, app icon and display name are now correct"
    This reverts commit 65231861d1d21352f53a68a57753bb81c80b548d.

--- myapp/lib/src/home.dart
+++ myapp/lib/src/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:bike_kollective/src_exports.dart';
-import 'navbar.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -21,7 +20,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 20;
+  int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
