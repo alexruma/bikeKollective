5bb7fa4f2bf0060dae31133711f0f4fe06f2d3fb
mxjeffers <30639610+mxjeffers@users.noreply.github.com>
2022-01-30 23:00:34 -0700

    Location and bike select
    Location is now working. When close to a bikes location, and option to select a bike becomes visible.

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Bike Kollective'),
+      home: Gmaps(),
     );
   }
 }
