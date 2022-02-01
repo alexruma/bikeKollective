a1332c0162a743c1072dc5e35a68595733a61670
mxjeffers <30639610+mxjeffers@users.noreply.github.com>
2022-02-01 15:33:42 -0700

    Changes to bike list
    Update bike list configuration

--- myapp/lib/src/gmaps.dart
+++ myapp/lib/src/gmaps.dart
@@ -114,8 +114,6 @@ class _GmapsState extends State<Gmaps> {
       });
     });
 
-    // //TEST
-    // bikes?.forEach((bike) { initMarker(bike);});
   setState(() { _markers;
 
   });
@@ -199,44 +197,46 @@ class _GmapsState extends State<Gmaps> {
                 itemCount: data.size,
                 itemBuilder: (context, index){
                   bikeLoc[data.docs[index].id] = data.docs[index]['location'];
-                  var dist = 5;
                   return
                     Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        decoration: BoxDecoration(borderRadius: const BorderRadius.all(Radius.circular(20)),
-                              border: Border.all(color: Colors.black12),
-                          color: Colors.grey,),
-                        width: 200,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [const Expanded(
-                              flex: 4,
-                              child: Image(image: AssetImage('assets/images/bike-icon.png'),
-                              height: 100
-                              ,)),
-                            Expanded(child: RatingStar(rating: data.docs[index]['rating']),
-                            flex: 1,),
-                            Padding(
-                              padding: const EdgeInsets.all(1.0),
-                              child: Expanded(flex: 1,
-                                  child: Row(
-                                    children: [
-                                      ElevatedButton(style: const ButtonStyle(),
-                                      onPressed: () {  },
-                                      child: const Text('View Bike',
-                                        style: TextStyle(color: Colors.black87),),),
+                      child: FittedBox(
+                        fit: BoxFit.contain,
+                        child: Container(
+                          decoration: BoxDecoration(borderRadius: const BorderRadius.all(Radius.circular(20)),
+                                border: Border.all(color: Colors.black12),
+                            color: Colors.grey,),
+                          width: 200,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [ Row(
+                               mainAxisAlignment: MainAxisAlignment.center,
+                                children: const [Image(image: AssetImage('assets/images/bike-icon.png'),
+                                height: 100
+                                ,)]),
+                              Row(mainAxisAlignment: MainAxisAlignment.center,
+                                  children:[ RatingStar(rating: data.docs[index]['rating']),
+                              ]),
+                              Padding(
+                                padding: const EdgeInsets.all(1.0),
+                                child: Row(
+                                  children: [
+                                    ElevatedButton(style: const ButtonStyle(),
+                                    onPressed: () {  },
+                                    child: const Text('View Bike',
+                                      style: TextStyle(color: Colors.black87),),),
 
-                                      // Will be used to select bike if within distance
-                                      if(closePoint[data.docs[index].id]??false)
-                                        ElevatedButton(onPressed: (){}, child:
-                                        const Text('Select Bike',
-                                          style: TextStyle(color: Colors.black54),))
-                                    ],
-                                  ) ),
-                            ),
-                          ],
-                        )
+                                    // Will be used to select bike if within distance
+                                    if(closePoint[data.docs[index].id]??false)
+                                      ElevatedButton(onPressed: (){}, child:
+                                      const Text('Select Bike',
+                                        style: TextStyle(color: Colors.black54),))
+                                  ],
+                                ),
+                              ),
+                            ],
+                          )
+                        ),
                       ),
                     );
                 });
