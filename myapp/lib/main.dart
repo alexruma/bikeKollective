bf687be43a2ca6abd30cac3e2782aa328ac879ac
Alex Ruma <alexanderruma@gmail.com>
2022-02-06 22:23:38 -0800

    navbar functionality and account page skeleton

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -4,9 +4,7 @@ import 'src_exports.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
-
-void main() async{
+void main() async {
   // Setting need to start firebase
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -15,26 +13,28 @@ void main() async{
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
   // This widget is the root of your application.
+
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Bike Kollective',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        textTheme: TextTheme(
-          headline5: GoogleFonts.pacifico(
-            textStyle: const TextStyle(
-              fontSize: 42,
-            )
-          )
-        )
-      ),
-      home: const AuthGate(),
+          primarySwatch: Colors.blue,
+          textTheme: TextTheme(
+              headline5: GoogleFonts.pacifico(
+                  textStyle: const TextStyle(
+            fontSize: 42,
+          )))),
+      home: const NavBarPage(title: "Nav"),
     );
   }
 }
