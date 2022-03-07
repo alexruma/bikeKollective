fbd8c75da8d5cf95e21ed89a5e2ab86c2e318d3f
lucaono13 <gionorato@gmail.com>
2022-03-06 20:41:52 -0800

    Added the ability to send the email verification

--- myapp/lib/models/emailAlert.dart
+++ myapp/lib/models/emailAlert.dart
@@ -4,7 +4,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
 
-emailAlert(context){
+emailAlert(context) async {
+  await FirebaseAuth.instance.currentUser?.sendEmailVerification();
   return showDialog(context: context,
       barrierDismissible: false,
       builder: (BuildContext context){
